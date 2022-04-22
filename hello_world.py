5a2c55046462a67b3f7fd1878486bac541163814
Johanna Salazar <104191365+josalazar2020@users.noreply.github.com>
2022-04-22 11:54:59 +0200

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,24 +1,2 @@
-# Hello GitHub
-
-A Fresh, New GitHub repo just waiting to be developed.  All contributors welcome!
-
-## Exercises
-
-### Make a new Issue, letting me know that there is a bug in the `hello_world.py` file.
-
-### Fork this repository
-
-### Add your initials to the readme below:
-
-My Initials:  ____________
-
-
-### Create a file called "hello_<yourname>.txt"  and type a text greeting inside.
-  
-  
-### Fix the Python bug in the `hello_world.py` file.
-  
-  
-### Make a pull request with these changes, with a polite and friendly message describing what you did!  Reference the issue you created by putting the issue number after the `#` sign.  (for example, "fixes issue #1").  
-  
- 
+# error: missing closing brackets
+print("Hello, world!")
